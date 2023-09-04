c27d9bbe44f2342a91177336bd20bd8ab0f1da99
Adrian Montesinos <netlinkbolivia@gmail.com>
2023-09-03 21:57:43 -0400

    app corrections

--- api-users/app.py
+++ api-users/app.py
@@ -11,11 +11,48 @@ import os
 # Cargar las Variables de entorno desde el archivo .env
 load_dotenv()
 
+# Determine the folder of the top-level directory of this project
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define la configuración de la aplicación
+class Config(object):
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
+    # Since SQLAlchemy 1.4.x has removed support for the 'postgres://' URI scheme,
+    # update the URI to the postgres database to use the supported 'postgresql://' scheme
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Logging
+    LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
+
+# Define la configuración de producción
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+# Define la configuración de desarrollo
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+# Define la configuración de pruebas
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
+    WTF_CSRF_ENABLED = False
+
+# Crea la aplicación Flask y establece su configuración
 
 app = Flask(__name__)
 CORS(app)
-database_uri = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+#database_uri = os.environ.get('DATABASE_URI')
+
+app.config.from_object(DevelopmentConfig)
+
 db = SQLAlchemy(app)
 
 class Usuario(db.Model):
